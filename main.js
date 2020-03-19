f3593d2647b066dcd5db467327ec54bd527cc2e0
Umberto Del Piano <umberdelpi1@gmail.com>
2020-03-19 14:31:07 +0100

    Definizione html e  commenti in .js

---
+++ main.js
@@ -0,0 +1,16 @@
+// Chiedere i KM che l'utente vuole percorrere
+
+
+// Età passeggero ( utente)
+
+
+// Il prezzo del biglietto è di ( 0,21€ al KM)
+
+
+// Calcolo prezzo totale del viaggio
+
+
+// 20% di sconto (Minorenni)
+
+
+// 40% di sconto (Over 65)
