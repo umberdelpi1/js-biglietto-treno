1f27189b9426599d79a5d3145f02917524d8d99c
Umberto Del Piano <umberdelpi1@gmail.com>
2020-03-19 17:18:05 +0100

    Esercizio finito

--- main.js
+++ main.js
@@ -5,23 +5,37 @@ var kmDaPercorrere = prompt("Quanti Km desideri percorrere?")
 var etaPasseggero = prompt("Inserisci la tua età")
 
 // Il prezzo del biglietto è di ( 0,21€ al KM)
-var costoBigliettoSenzaSconti = (0.21 * kmDaPercorrere);
+var costoBigliettoSenzaSconti = 0.21 * kmDaPercorrere;
+
+// Biglieto con sconto
+
+// 20% di sconto (Minorenni)
+var bigliettoScontoMinorenni = ((costoBigliettoSenzaSconti * 100) / 120);
+
+
+// 40% di sconto (Over 65)
+var bigliettoScontiAnziani = (costoBigliettoSenzaSconti * 100) / 140;
 
 
 
+if (etaPasseggero < 18) {
+costoBigliettoSenzaSconti = bigliettoScontoMinorenni
+} else if (etaPasseggero > 65) {
+costoBigliettoSenzaSconti = bigliettoScontiAnziani
+}
 
+
+// Calcolo prezzo totale del viaggio
 console.log("Km che utente percorre: " + kmDaPercorrere);
 console.log("Età passeggero: " + etaPasseggero);
 console.log("Costo biglietto senza sconti €: " + costoBigliettoSenzaSconti);
 
+// Out-put
+if (etaPasseggero < 18){
+  document.getElementById("biglietto").innerHTML= "Il costo finale del tuo biglietto è: " + costoBigliettoSenzaSconti;
+}
 
-
-
-
-// Calcolo prezzo totale del viaggio
-
-
-// 20% di sconto (Minorenni)
-
-
-// 40% di sconto (Over 65)
+else if (etaPasseggero > 65) {
+  document.getElementById("biglietto").innerHTML= "Pezzo di merda vecchiaccio della mal'ora: " + costoBigliettoSenzaSconti;
+}
+else {document.getElementById("biglietto").innerHTML= "Sfigato ritenta sarai più fortunato non hai sconti prezzo pieno:( by Made in Sud) " + costoBigliettoSenzaSconti;}
