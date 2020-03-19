aeeac30af8169fb1015efdb6e5cef0f9ed75cfc8
Umberto Del Piano <umberdelpi1@gmail.com>
2020-03-19 15:26:29 +0100

    Costo biglietto totale finito

--- main.js
+++ main.js
@@ -1,10 +1,21 @@
 // Chiedere i KM che l'utente vuole percorrere
-
+var kmDaPercorrere = prompt("Quanti Km desideri percorrere?")
 
 // Età passeggero ( utente)
-
+var etaPasseggero = prompt("Inserisci la tua età")
 
 // Il prezzo del biglietto è di ( 0,21€ al KM)
+var costoBigliettoSenzaSconti = (0.21 * kmDaPercorrere);
+
+
+
+
+console.log("Km che utente percorre: " + kmDaPercorrere);
+console.log("Età passeggero: " + etaPasseggero);
+console.log("Costo biglietto senza sconti €: " + costoBigliettoSenzaSconti);
+
+
+
 
 
 // Calcolo prezzo totale del viaggio
